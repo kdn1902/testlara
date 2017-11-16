1d35fbcdf9acbe7fc025b89dcbd9a230468c52fb
unknown <kdn@kdn.local.kolbiko>
2017-11-16 16:57:57 +0300

    View tuning

--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -3,10 +3,40 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
+                    <div class="panel-heading">Dimon Example Component</div>
 
                     <div class="panel-body">
                         I'm an example component!
+                     <address>
+ 						 <strong>Twitter, Inc.</strong><br>
+					 	 795 Folsom Ave, Suite 600<br>
+ 						 San Francisco, CA 94107<br>
+  						 <abbr title="Phone">P:</abbr> (123) 456-7890
+					</address>
+
+					<address>
+					  <strong>Full Name</strong><br>
+					  <a href="mailto:#">first.last@example.com</a>
+					</address>
+					<button type="button" class="btn btn-default">Default</button>
+
+					<!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+					<button type="button" class="btn btn-primary">Primary</button>
+
+					<!-- Indicates a successful or positive action -->
+					<button type="button" class="btn btn-success">Success</button>
+
+					<!-- Contextual button for informational alert messages -->
+					<button type="button" class="btn btn-info">Info</button>
+
+					<!-- Indicates caution should be taken with this action -->
+					<button type="button" class="btn btn-warning">Warning</button>
+
+					<!-- Indicates a dangerous or potentially negative action -->
+					<button type="button" class="btn btn-danger">Danger</button>
+
+					<!-- Deemphasize a button by making it look like a link while maintaining button behavior -->
+					<button type="button" class="btn btn-link">Ссылка</button>
                     </div>
                 </div>
             </div>
