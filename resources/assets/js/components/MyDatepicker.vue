2faec2386d9134c4933861355fc8575955833cbc
Kosinov Dmitry <kdn1902@gmail.com>
2017-12-19 16:21:42 +0300

    add foto2

---
+++ resources/assets/js/components/MyDatepicker.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="dp">
+        <datepicker v-model="bd" language="ru" @selected="changeData"></datepicker>
+  </div>
+</template>
+
+<script>
+import Datepicker from 'vuejs-datepicker'
+
+export default
+{
+  name: 'dp',
+  components: {Datepicker},
+  props:['ddate'],
+  data (){
+  		return { bd: this.ddate};
+  	},
+  methods:{
+  	changeData: function (newdate)
+  	{
+  		this.$emit('changedate', newdate.toLocaleFormat('%Y-%m-%d'))		
+  	}
+  }
+}
+</script>
